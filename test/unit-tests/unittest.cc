0e2976781cf4218f3ea410e2c44ee7a90e83d592
Philipp <philipp@lrde.epita.fr>
2019-10-31 13:17:32 +0100

    ..

---
+++ test/unit-tests/unittest.cc
@@ -0,0 +1,17 @@
+/*
+ * This file is a part of the TChecker project.
+ *
+ * See files AUTHORS and LICENSE for copyright details.
+ *
+ */
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include "test-db.hh"
+#include "test-dbm.hh"
+#include "test-extract_variables.hh"
+#include "test-guard_weak_sync.hh"
+#include "test-offset_clock_variables.hh"
+#include "test-offset_dbm.hh"
+#include "test-variables-access.hh"
